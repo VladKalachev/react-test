38a0f11f004d140479ace98f2e619c91c2f63203
Corvax <vlvete1990@yandex.ru>
2022-02-13 20:40:16 +0300

    chore: test for component List

---
+++ src/components/List/List.jsx
@@ -0,0 +1,17 @@
+const List = (props) => {
+  const {items = []} = props;
+
+  if(!items.length) return null;
+
+  return (
+    <ul>
+      {
+        items.map(el => (
+          <li key={el}>{el}</li>
+        ))
+      }
+    </ul>
+  )
+}
+
+export default List;
