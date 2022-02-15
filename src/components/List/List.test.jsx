548d495fa2f54ac9051e3397436a0dcc8f644290
Corvax <vlvete1990@yandex.ru>
2022-02-15 19:52:34 +0300

    chore: snapshots

--- src/components/List/List.test.jsx
+++ src/components/List/List.test.jsx
@@ -11,10 +11,21 @@ describe('List component', () => {
     expect(screen.getByText('html')).toBeInTheDocument();
   })
 
-  
   it('List renders without data', () => {
     render(<List />);
 
     expect(screen.queryByRole('list')).toBeNull();
   })
+
+  it('List snapshot', () => {
+    const list = render(<List items={data} />);
+    expect(list).toMatchSnapshot();
+  })
+
+  it('List empty snapshot', () => {
+    const list = render(<List />);
+    expect(list).toMatchSnapshot();
+  })
+
+
 });
