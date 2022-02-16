ba0243141a79a6e738ffefb03fc1299d60f3b94b
Corvax <vlvete1990@yandex.ru>
2022-02-16 20:21:15 +0300

    chore: integration tests

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,13 +11,17 @@ const data = [
 
 function App() {
   const [search, setSearch] = useState('')
+  const [items, setItems] = useState(data);
+  useEffect(() => {
+    setItems(data.filter(el => el.toLowerCase().includes(search.toLowerCase())));
+  }, [search])
   return (
     <div className="App">
       <header className="App-header">
         <Search value={search} onChange={(e) => setSearch(e.target.value) }>
           Find course:
         </Search>
-        <List items={data} />
+        <List items={items} />
       </header>
     </div>
   );
--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 
@@ -7,5 +8,27 @@ describe('App component', () => {
     render(<App />);
 
     expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getByText('Find course:')).toBeInTheDocument();
+  })
+
+  it('typing in Searchbox works', () => {
+    render(<App />);
+
+    expect(screen.queryByDisplayValue(/HTML/)).toBeNull();
+
+    userEvent.type(screen.getByRole('textbox'), 'React');
+
+    expect(screen.queryByDisplayValue(/React/)).toBeInTheDocument();
+  });
+
+  it('search filter is working', () => {
+    render(<App />);
+
+    expect(screen.getByText(/HTML/)).toBeInTheDocument();
+    expect(screen.getByText(/CSS/)).toBeInTheDocument();
+
+    userEvent.type(screen.getByRole('textbox'), 'script');
+
+    expect(screen.queryByText(/HTML/)).toBeNull();
   })
 });
